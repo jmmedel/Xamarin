67618143aafb48ce9740778f766c8bc744635306
john <jm_sasuke25@yahoo.com>
2018-01-26 03:30:12 +0900

    Not Done

---
+++ T_001_Hello_Worlds/T_001_Hello_Worlds/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace T_001_Hello_Worlds
+{
+    [Activity(Label = "T_001_Hello_Worlds", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
