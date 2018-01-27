f3688792c0b95e214357966c8b40be42487cb75c
john <jm_sasuke25@yahoo.com>
2018-01-27 12:33:25 +0900

    Not_Done

---
+++ T_002_ListView/T_002_ListView/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace T_002_ListView
+{
+    [Activity(Label = "T_002_ListView", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private List<string> mitems;
+        private ListView mlistviews;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mlistviews = FindViewById<ListView>(Resource.Id.MyListview);
+
+            mitems = new List<string>();
+            mitems.Add("John");
+            mitems.Add("Medel");
+            mitems.Add("Kagaya");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mitems);
+            mlistviews.Adapter = adapter;
+
+        }
+    }
+}
+
