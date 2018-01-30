e949649bbcbcbccc5a5cde3d3616cc6f1c0afcb2
john <jm_sasuke25@yahoo.com>
2018-01-31 01:54:17 +0900

    Done

--- T_002_ListView/T_002_ListView/MainActivity.cs
+++ T_002_ListView/T_002_ListView/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
+using System;
 
 namespace T_002_ListView
 {
@@ -27,12 +28,36 @@ namespace T_002_ListView
             john.Gender = "Male";
             john.Age = 24;
             mitems.Add(john);
+            Person john2 = new Person();
+            john2.FirstName = "Jh";
+            john2.LastName = "medel";
+            john2.Gender = "Male";
+            john2.Age = 21;
+            mitems.Add(john2);
+            Person john3 = new Person();
+            john3.FirstName = "John";
+            john3.LastName = "Med";
+            john3.Gender = "Male";
+            john3.Age = 18;
+            mitems.Add(john3);
             // the default listview
             //ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mitems);
             // call created mylistView class to make use your listview
             MyListview adapter = new MyListview(this, mitems);
             mlistviews.Adapter = adapter;
+            mlistviews.ItemClick += mlistviews_ItemClick;
+            mlistviews.ItemLongClick += mlistviews_ItemLongClick;
 
+        }
+
+        private void mlistviews_ItemLongClick(object sender, AdapterView.ItemLongClickEventArgs e)
+        {   // testing to see of the event is working
+            Console.WriteLine(mitems[e.Position].LastName);
+        }
+
+        private void mlistviews_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {   // testing to see of the event is working
+            Console.WriteLine(mitems[e.Position].FirstName);
         }
     }
 }
