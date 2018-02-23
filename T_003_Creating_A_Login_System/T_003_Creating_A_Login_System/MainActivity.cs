efcd3fe09b956670c2ad5669bb402361c3eed24a
john <jm_sasuke25@yahoo.com>
2018-02-24 00:47:41 +0900

    Done

--- T_003_Creating_A_Login_System/T_003_Creating_A_Login_System/MainActivity.cs
+++ T_003_Creating_A_Login_System/T_003_Creating_A_Login_System/MainActivity.cs
@@ -10,9 +10,10 @@ namespace T_003_Creating_A_Login_System
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+               // this is the main
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
         }
     }
 }
