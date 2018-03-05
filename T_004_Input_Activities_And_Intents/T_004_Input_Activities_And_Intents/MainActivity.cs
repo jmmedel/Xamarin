0a576e2559ad5eea5423e7735c51795b0de5136a
john <jm.kagaya@gmail.com>
2018-03-05 23:44:50 +0900

    Done

---
+++ T_004_Input_Activities_And_Intents/T_004_Input_Activities_And_Intents/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace T_004_Input_Activities_And_Intents
+{
+    [Activity(Label = "T_004_Input_Activities_And_Intents", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
