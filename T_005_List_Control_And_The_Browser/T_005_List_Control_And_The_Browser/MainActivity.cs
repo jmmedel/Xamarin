f45a30937bebcd6bd06560255125a012033ef000
john <jm.kagaya@gmail.com>
2018-03-06 00:29:29 +0900

    Not
    Done

---
+++ T_005_List_Control_And_The_Browser/T_005_List_Control_And_The_Browser/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace T_005_List_Control_And_The_Browser
+{
+    [Activity(Label = "T_005_List_Control_And_The_Browser", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
