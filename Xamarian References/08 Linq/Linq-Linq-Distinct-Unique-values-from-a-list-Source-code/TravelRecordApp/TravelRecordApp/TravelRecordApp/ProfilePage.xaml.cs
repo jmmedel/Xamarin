8867a274df6d002c18d309672b839618ec4c94dd
kagaya25 <jm.kagaya@gmail.com>
2018-05-19 06:24:40 +0900

    Done
    Done

---
+++ Xamarian References/08 Linq/Linq-Linq-Distinct-Unique-values-from-a-list-Source-code/TravelRecordApp/TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProfilePage : ContentPage
+	{
+		public ProfilePage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+
+                var categories = (from p in postTable
+                                  orderby p.CategoryId
+                                  select p.CategoryName).Distinct().ToList();
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
+        }
+    }
+}
---
+++ Xamarian References/08 Linq/Linq-Profile-Page-Source-code/TravelRecordApp/TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProfilePage : ContentPage
+	{
+		public ProfilePage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
+        }
+    }
+}
