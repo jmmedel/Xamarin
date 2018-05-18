83e3a489de4ef0273fd4166e3e2fc25f57122723
kagaya25 <jm.kagaya@gmail.com>
2018-05-19 06:29:04 +0900

    Done
    Done

---
+++ Xamarian References/06 REST Consuming Foursquare/REST-Consuming-Foursquare-Services-Deserializing-Json-strings-into-C#-objects-/TravelRecordApp/TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Plugin.Geolocator;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Logic;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        public NewTravelPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync();
+
+            var venues = await VenueLogic.GetVenues(position.Latitude, position.Longitude);
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = expretienceEntry.Text
+            };
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
+
+                if (rows > 0)
+                    DisplayAlert("Success", "Experience succesfully inserter", "Ok");
+                else
+                    DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
+            }
+        }
+    }
+}
